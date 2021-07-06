35dd0d82ffe48526af58c129ae3c2d90f8510f8c
Stefan Frade <stefan.henrique@outlook.com>
2021-07-06 10:23:30 -0300

    yml

---
+++ test.php
@@ -0,0 +1,18 @@
+<?php 
+/**
+ * File doc
+ * PHP version 7.4
+ * 
+ * @category Category
+ * @package  Package
+ * @author   Octavio <octavio.marchi@gmail.com>
+ * @license  http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @link     5
+ */
+
+function a(){ return 2; }
+
+$a = random_int(1,10);
+if($a < 5){
+	echo "aha";
+}
