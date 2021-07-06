393552eb9fff16a584eedc8ce189a9698fe8006d
Stefan Frade <stefan.henrique@outlook.com>
2021-07-06 12:09:24 -0300

    hm

--- newtest.php
+++ newtest.php
@@ -2,16 +2,17 @@
 /**
  * File doc
  * PHP version 7.4
- * 
+ *
  * @category Category
  * @package  Package
  * @author   Octavio <octavio.marchi@gmail.com>
  * @license  http://www.opensource.org/licenses/mit-license.html  MIT License
  * @link     5
  */
+
 /**
  * Class doc
- * 
+ *
  * @category Category
  * @package  Package
  * @author   Octavio <octavio.marchi@gmail.com>
@@ -21,4 +22,4 @@
 
 
 
-echo "A";
+echo 'A';
