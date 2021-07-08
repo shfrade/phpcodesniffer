d78c6cdb6a854c9dee262b7c17bc0999e8395691
Stefan Frade <stefan.henrique@outlook.com>
2021-07-08 14:09:49 -0300

    a

---
+++ phpwithproblem.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace base;
+
+/**
+ * File doc
+ * PHP version 7.4
+ *
+ * @category Category
+ * @package  Package
+ * @author   Octavio <octavio.marchi@gmail.com>
+ * @license  http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @link     5
+ */
+
+/**
+ * Class doc
+ *
+ * @category Category
+ * @package  Package
+ * @author   Octavio <octavio.marchi@gmail.com>
+ * @license  http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @link     5
+ */
+
+class umtest
+{
+	private $nome;
+
+	public function getNome()
+	{
+	return $this->nome;
+	}
+
+	public function setNome($nome){
+	$this->nome = $nome;
+	    return;
+	}
+}
